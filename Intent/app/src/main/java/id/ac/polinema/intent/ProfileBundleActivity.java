306dd8a40f4839ae2ee21703b216c6a09b12bee0
haryobagus <haryobagus14@gmail.com>
2019-11-07 12:42:23 +0700

    initial commit

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -11,7 +11,6 @@ public class ProfileBundleActivity extends AppCompatActivity {
     private TextView ageText;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
